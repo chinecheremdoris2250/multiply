9a89cfdcd86f1ddc4160914dead1beaec47a7ab7
Nechenwa <chinecheremdoris2250@gmail.com>
2022-09-20 11:59:54 +0100

    first commit

---
+++ i.js
@@ -0,0 +1,10 @@
+const num1 = Math.ceil(Math.random() * 10)
+
+const num2 = Math.ceil(Math.random() * 10)
+
+const questionEl = document.getElementById("question");
+
+questionEl.innerText = 'what is ${num1} multiply by ${num2}?'
+
+
+
